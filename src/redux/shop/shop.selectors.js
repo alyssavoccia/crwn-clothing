0d97ab1bbef2d5541e74da6cbb5a33c472097e33
Alyssa Voccia <avoccia@hotmail.com>
2021-12-13 15:48:23 -0500

    Updated the directory component and the routing to the different collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,4 @@
-import { memoize } from "lodash.memoize";
 import { createSelector } from "reselect";
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
 
 const selectShop = state => state.shop;
 
@@ -16,7 +7,12 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
-export const selectCollection = (collectionUrlParam) => createSelector(
+export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  (collections) => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
 );
